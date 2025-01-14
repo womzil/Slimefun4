c071894e57cd402cade3011e85c641763822a24d
StarWishsama <starwishsama@outlook.com>
2025-01-14 14:32:39 +0800

    fix: actual fix for #994

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/BlockListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/BlockListener.java
@@ -119,6 +119,11 @@ public class BlockListener implements Listener {
         }
 
         if (sfItem != null && !(sfItem instanceof NotPlaceable)) {
+            // Fixes #994, should check placed block is equals to item material or not.
+            if (item.getType() != e.getBlock().getType()) {
+                return;
+            }
+
             if (!sfItem.canUse(e.getPlayer(), true)) {
                 e.setCancelled(true);
             } else {
