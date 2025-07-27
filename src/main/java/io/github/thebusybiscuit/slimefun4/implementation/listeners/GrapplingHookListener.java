8e77f1e223d271eeed96823ad457ce0b532669ad
ybw0014 <i@guizhanss.com>
2025-07-26 20:40:00 -0400

    feat(WoodcutterAndroid): 支持苍白橡木 (#1084)

--- src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/GrapplingHookListener.java
+++ src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/GrapplingHookListener.java
@@ -163,7 +163,7 @@ public class GrapplingHookListener implements Listener {
             return;
         }
 
-        for (GrapplingHookEntity hook : activeHooks.values()){
+        for (GrapplingHookEntity hook : activeHooks.values()) {
             if (hook.getLeashTarget() == e.getEntity()) {
                 e.setDropLeash(false);
                 return;
